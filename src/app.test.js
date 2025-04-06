7ea0a6f7dbc114828315538ff922a46ad803f5c9
petchluvsyou <petchluvsyou@outlook.com>
2025-04-06 17:20:49 +0700

    test

--- src/app.test.js
+++ src/app.test.js
@@ -3,7 +3,8 @@ const { dayOfTheWeek, monthOfTheYear } = require('./app');
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
-    expect( day ).toBe('Wednesday');
+    expect(day).toBe('Wednesday');
+    
 });
 
 test('getDay returns the month of the year'), () => {
